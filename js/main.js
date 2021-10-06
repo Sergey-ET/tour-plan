8e29b2119c0455448f54658785e57e3e46a792c5
Sergey-ET <sergey_troshkin@hotmail.com>
2021-10-06 17:59:13 +0300

    Create: add API Yandex map

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ const swiper = new Swiper('.swiper', {
     onlyInViewport: false,
   },
 })
+
+ymaps.ready(init)
+function init() {
+  var myMap = new ymaps.Map(
+      'map',
+      {
+        center: [7.89073, 98.294727],
+        zoom: 16,
+      },
+      {
+        searchControlProvider: 'yandex#search',
+      },
+    ),
+    myPlacemark = new ymaps.Placemark(
+      [7.89073, 98.294727],
+      {
+        // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+        hintContent: 'Grand Hilton Hotel',
+      },
+      {
+        preset: 'islands#redIcon',
+      },
+    )
+
+  myMap.geoObjects.add(myPlacemark)
+}
