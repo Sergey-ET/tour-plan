a8598ce34bcc73446dc35e69656bd3a2015e30fe
Sergey-ET <sergey_troshkin@hotmail.com>
2021-10-11 10:15:52 +0300

    Replace API Yandex.Map with google map

--- js/main.js
+++ js/main.js
@@ -24,29 +24,3 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 })
-
-ymaps.ready(init)
-function init() {
-  var myMap = new ymaps.Map(
-      'map',
-      {
-        center: [7.89073, 98.294727],
-        zoom: 16,
-      },
-      {
-        searchControlProvider: 'yandex#search',
-      },
-    ),
-    myPlacemark = new ymaps.Placemark(
-      [7.89073, 98.294727],
-      {
-        // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
-        hintContent: 'Grand Hilton Hotel',
-      },
-      {
-        preset: 'islands#redIcon',
-      },
-    )
-
-  myMap.geoObjects.add(myPlacemark)
-}
